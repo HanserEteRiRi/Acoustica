d72e050dca155087d1a88988b9e9e6ca10fa0163
HanserEteRiRi <1736369624@qq.com>
2023-04-19 11:09:45 +0800

    feat: 更改项目名

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "/api", //被代理到http://localhost:3000
+  baseURL: "/api", //被代理到服务器
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,8 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000", // 目标 API 服务器地址
+        // target: "http://localhost:3000", // 本地测试
+        target: "http://43.138.199.62:3000", // 服务器
         changeOrigin: true, // 允许改变 Origin
         pathRewrite: {
           "^/api": "", // 重写请求，将/api替换为空字符串
